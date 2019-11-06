6d4fb40013068229bfea1a3d5e921a65104391a0
ImDaeseong <cs93059@gmail.com>
2019-11-06 10:25:35 +0900

    RestApi_test

--- client.go
+++ client.go
@@ -11,7 +11,84 @@ import (
 
 func getAllList() bool {
 
+	fmt.Println("--------------------Call getAllList-----------------------")
+
 	defaulturl := "http://127.0.0.1:8080/api/AllList"
+	sUrl := fmt.Sprintf("%s", defaulturl)
+
+	res, err := http.Get(sUrl)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+
+	fmt.Println(string(body))
+
+	return true
+}
+
+func getItem1List() bool {
+
+	fmt.Println("--------------------Call getItem1List-----------------------")
+
+	defaulturl := "http://127.0.0.1:8080/api/item/1"
+	sUrl := fmt.Sprintf("%s", defaulturl)
+
+	res, err := http.Get(sUrl)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+
+	fmt.Println(string(body))
+
+	return true
+}
+
+func getItem2List() bool {
+
+	fmt.Println("--------------------Call getItem2List-----------------------")
+
+	defaulturl := "http://127.0.0.1:8080/api/item/2"
+	sUrl := fmt.Sprintf("%s", defaulturl)
+
+	res, err := http.Get(sUrl)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+
+	fmt.Println(string(body))
+
+	return true
+}
+
+func getItem3List() bool {
+
+	fmt.Println("--------------------Call getItem3List-----------------------")
+
+	defaulturl := "http://127.0.0.1:8080/api/item/3"
 	sUrl := fmt.Sprintf("%s", defaulturl)
 
 	res, err := http.Get(sUrl)
@@ -36,7 +113,6 @@ func main() {
 
 	go func() {
 		for {
-			fmt.Println("--------------------첫번째 요청-----------------------")
 			getAllList()
 			time.Sleep(100 * time.Millisecond)
 		}
@@ -44,24 +120,21 @@ func main() {
 
 	go func() {
 		for {
-			fmt.Println("--------------------두번째 요청-----------------------")
-			getAllList()
+			getItem1List()
 			time.Sleep(500 * time.Millisecond)
 		}
 	}()
 
 	go func() {
 		for {
-			fmt.Println("--------------------세번째 요청-----------------------")
-			getAllList()
+			getItem2List()
 			time.Sleep(100 * time.Millisecond)
 		}
 	}()
 
 	go func() {
 		for {
-			fmt.Println("--------------------네번째 요청-----------------------")
-			getAllList()
+			getItem3List()
 			time.Sleep(500 * time.Millisecond)
 		}
 	}()
